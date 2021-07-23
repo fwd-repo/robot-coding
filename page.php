f72b3a7c7a769d7d29272ebbbf7881d977706059
Ed Tansey <office.fwd@yahoo.com>
2021-07-23 14:48:55 +0100

    Create contact page

--- page.php
+++ page.php
@@ -23,8 +23,7 @@ while(have_posts()) {
 	<?php } ?>
 
 	<aside id="sidebar">
-		<h3>Sidebar Heading</h3>
-		<p>Sidebar Content</p>
+		<?php dynamic_sidebar('main_sidebar'); ?>
 	</aside>
 </div>
 <?php get_footer(); ?>
